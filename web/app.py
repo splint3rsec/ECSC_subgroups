df0e3e66d8e5ede8b21f70cde4a04fef50b65974
splnintersec <khalil.lem@protonmail.com>
2022-04-25 23:25:27 +0200

    added 404 handling and get param

--- web/app.py
+++ web/app.py
@@ -1,7 +1,8 @@
-from flask import Flask
-from flask import render_template
+from re import template
+from flask import Flask, request, render_template, render_template_string
 
 app = Flask(__name__)
+app.run(debug=True)
 
 @app.route('/')
 def first():
@@ -9,4 +10,10 @@ def first():
 
 @app.route('/hidden')
 def hidden():
+    # if request.method == 'GET':
+    #     name = request.args.get('query')
     return render_template('secret.html')
+
+@app.errorhandler(404)
+def notfound(dir):
+    return render_template("notfound.html"), 404
